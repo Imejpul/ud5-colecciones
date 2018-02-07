954628576ef68525b1229ad6ef886bd9d5bf14ef
Unknown <ivanmejiapulido@gmail.com>
2018-02-07 20:31:34 +0100

    Ejercicio 5
    A medio hacer.

---
+++ 05_personas/personas/src/com/imejpul/Main.java
@@ -0,0 +1,22 @@
+package com.imejpul;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("---Introduce datos de grupo de personas 1");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Introduzca Datos persona");
+            System.out.print("Nombre: " );
+
+        }
+
+
+        //set<Persona> personas = new HashSet<>();
+    }
+}
---
+++ 05_personas/personas/src/com/imejpul/Persona.java
@@ -0,0 +1,47 @@
+package com.imejpul;
+
+import java.util.Objects;
+
+public class Persona {
+
+    private String dni;
+    private String nombre;
+
+    public Persona() {
+    }
+
+    public Persona(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(dni, persona.dni);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(dni);
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
