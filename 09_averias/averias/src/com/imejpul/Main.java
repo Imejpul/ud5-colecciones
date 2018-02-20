3d1c868538fb594f360a92d592e0f3fac8b7b302
Unknown <ivanmejiapulido@gmail.com>
2018-02-20 16:17:30 +0100

    Ejer9 (100%_sin probar)

--- 09_averias/averias/src/com/imejpul/Main.java
+++ 09_averias/averias/src/com/imejpul/Main.java
@@ -36,28 +36,126 @@ public class Main {
             switch (opcion) {
                 case 1:
                     //pedir nombre técnico y almacenar en variable
+                    System.out.println("--Datos Técnico--");
+
+                    System.out.print("Nombre: ");
+                    String nombre = br.readLine();
 
                     //pedir nivel técnicmo y almacenar en variable
+                    System.out.print("Nivel: ");
+                    int nivel = Integer.parseInt(br.readLine());
 
                     //añadir a lista general de técnicos (inicializando constructor con parámetros pedidos)
+                    tecnicos.add(new Tecnico(nombre, nivel));
                     break;
 
                 case 21:
+                    //pedir título, descripción, fecha_apertura, nivel y almacenar en variables
+                    System.out.println("--Datos Avería--");
+
+                    System.out.print("Título: ");
+                    String titulo = br.readLine();
+
+                    System.out.print("Descripción: ");
+                    String descrip = br.readLine();
+
+                    System.out.print("Fecha Apertura: ");
+                    String fechaAp = br.readLine();
+
+                    System.out.print("Nivel: ");
+                    int nivelAv = Integer.parseInt(br.readLine());
+
+                    //añadir a lista general averías (inicializando constructor con parámetros pedidos)
+                    averias.add(new Averia(titulo, descrip, fechaAp, nivelAv));
+
+                    //(comprobar que se añade al final de la LinkedList).
                     break;
 
                 case 22:
+                    //obtener la referencia del técnico al que le será asignada (mostrar lista técnicos disponibles)
+                    for (int i = 0; i < tecnicos.size(); i++) {
+                        System.out.println((i + 1) + " " + tecnicos.get(i));
+                    }
+
+                    System.out.print("Introduzca número técnico de los listados: ");
+                    int indTecnico = Integer.parseInt(br.readLine()) - 1;
+
+                    //añadir la avería a la lista de averías del técnico (al final)
+                    tecnicos.get(indTecnico).getAveriasTecnico().add(averias.get(0));
+
+                    //añadir el técnico a la lista de técnicos de la avería
+                    averias.get(0).getTecnicosAveria().add(tecnicos.get(indTecnico));
+
+                    System.out.println("Asignada Avería.");
                     break;
 
                 case 23:
+                    //elegir técnico (obtener índice)
+                    for (int i = 0; i < tecnicos.size(); i++) {
+                        System.out.println((i + 1) + " " + tecnicos.get(i));
+                    }
+
+                    System.out.print("Introduzca número técnico de los listados: ");
+                    indTecnico = Integer.parseInt(br.readLine()) - 1;
+
+                    Averia a;
+                    a = tecnicos.get(indTecnico).getAveriasTecnico().get(0);
+
+                    Tecnico t;
+                    t = tecnicos.get(indTecnico);
+
+                    //si no se resuelve:
+                    if (a.getNivel() > t.getNivel()) {
+                        a.setNivel(a.getNivel() - 1);   //se baja el nivel de la avería una unidad
+
+                        averias.remove(a);  //se quita la avería de la lista general de pendientes
+                        averias.add(a);     //se añade de nuevo a la lista general para enviarla al final
+                    } else {
+                        averias.remove(a);
+                    }
                     break;
 
                 case 31:
+                    //saber el técnico y obtener índice
+                    for (int i = 0; i < tecnicos.size(); i++) {
+                        System.out.println((i + 1) + " " + tecnicos.get(i));
+                    }
+
+                    System.out.print("Introduzca número técnico de los listados: ");
+                    indTecnico = Integer.parseInt(br.readLine()) - 1;
+
+                    //si la avería está en la lista general y en la lista del técnico, no está resuelta
+                    for (Averia av : tecnicos.get(indTecnico).getAveriasTecnico()) {
+                        if (averias.contains(av)) {
+                            System.out.println(av);
+                        }
+                    }
                     break;
 
                 case 32:
+                    //saber técnico (obtener índice)
+                    for (int i = 0; i < tecnicos.size(); i++) {
+                        System.out.println((i + 1) + " " + tecnicos.get(i));
+                    }
+
+                    System.out.print("Introduzca número técnico de los listados: ");
+                    indTecnico = Integer.parseInt(br.readLine()) - 1;
+
+                    //si la avería está en la lista general y en la lista del técnico, no está resuelta
+                    for (Averia av : tecnicos.get(indTecnico).getAveriasTecnico()) {
+                        System.out.println(av);
+                    }
                     break;
 
                 case 33:
+                    //recorrer todos los técnicos
+                    for (Tecnico tec : tecnicos) {
+                        System.out.println(tec);
+                        for (Averia ave : averias) {
+                            System.out.println(ave);
+                        }
+                        System.out.println("------------");
+                    }
                     break;
 
                 case 4:
